c9863c4300833cebb3f1fb7db4d3f8207ba27453
komoooo <32304507+komoooo@users.noreply.github.com>
2017-09-26 18:23:41 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace rectanglearea
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			double a = double.Parse (Console.ReadLine ());
+			double b = double.Parse (Console.ReadLine ());
+
+			double sum = (a * b);
+
+			Console.WriteLine ("{0:F2}",sum);
+		}
+	}
+}
